4890b4c70a3894f398bb75b6d1e3998240844bd9
suman <tiwarisuman80@gmail.com>
2021-06-08 10:12:34 +0545

    code refactor

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
-// Generate the board
-$('form').on('submit', function(event){
-    boardLength = $('.board-length').val();
-    $(this).hide();
-    var widthHeight = parseInt(100/boardLength);
-    event.preventDefault();
-    $('h1').prepend(boardLength+ "X" + boardLength)
-    for (var i=1; i <= boardLength; i++){
-
-        // $('#selected-board').append('<div class="" style="height:' + heightSize + '; " id=' + "row" + i + ' data-value=' + boardLength + '></div>');
-        // for(var j=1; j <=boardLength; j++){
-        //     $('#row'+i).append('<span class="cell cell-styles" style="font-size: ' + fontSize + '; width:' + widthSize + '; height: '+ heightSize + '; " data-value='+ i+j+' ></span>')
-        // }
-        $('#selected-board').append('<tr class="" style="height: 50px; " id=' + "row" + i + ' data-value=' + boardLength + '></tr>');
-        for(var j=1; j <=boardLength; j++){
-            $('#row'+i).append('<td class="cell cell-styles" data-value='+ i+j+' ></td>')
-        }
-    }
-});
-
-$('#selected-board').on('click', '.cell', function(){
-    setClickEventForBoxes(this)
-});
-
-// initializeVariables();
+// initialize global variables
 var player = "O";
 var clickCount = 0;
 var winner;
@@ -30,16 +6,74 @@ var boardLength;
 displayCurrentPlayer();
 selectPlayer();
 
+function getPlayerToken(){
+    return player
+}
+
+function setPlayerToken(inputToken){
+    player = inputToken
+}
+
+function getClickCount(){
+    return clickCount
+}
+
+function getBoardLength(){
+    return boardLength
+}
+
+// Generate the board after submission of board length
+$('form').on('submit', function(event){
+    boardLength = $('.board-length').val();
+    // hide form
+    $(this).hide();
+    // show reset button
+    $('.resetBtn').removeClass('d-none');
+
+    displayBoardLengthHeader();
+    // prevent click event on submit
+    event.preventDefault();
+
+    generateBoard(getBoardLength());
+});
+
+function generateBoard(boardLength){
+    for (var i=1; i <= boardLength; i++){
+        $('#selected-board').append('<tr class="" style="height: 50px; " id=' + "row" + i + ' data-value=' + boardLength + '></tr>');
+        for(var j=1; j <=boardLength; j++){
+            $('#row'+i).append('<td class="cell cell-styles" data-value='+ i+j+' ></td>')
+        }
+    }
+}
+
+function displayBoardLengthHeader(){
+    $('h1').prepend(getBoardLength()+ "X" + getBoardLength());
+}
+
+// add click method for dynamically generated cells
+$('#selected-board').on('click', '.cell', function(){
+    setClickEventForBoxes(this)
+});
+
+// reset board
+$('.resetBtn').one('click', function(){
+    window.location.reload();
+})
+
 function setClickEventForBoxes(cell){
     $('#choose-player').addClass('d-none');
     cell.innerText = player;
     cell.classList.remove('cell')
 
     clickCount += 1;
-    setPlayer();
-    // if number of clicks is 5 or more then only check the winner
-    setTimeout(function () {
-        if (clickCount >= ((boardLength * 2) - 1)) {
+    changePlayer();
+    setTimeOutToCheckWinner(cell);
+}
+
+function setTimeOutToCheckWinner(cell){
+     // if number of clicks is 5 or more then only check the winner
+     setTimeout(function () {
+        if (clickCount >= ((getBoardLength() * 2) - 1)) {
             checkRow(getDataAttrValue(cell));
             // check column only if winner is undefined
             winner == undefined ? checkColumn(getDataAttrValue(cell)) : true;
@@ -48,11 +82,10 @@ function setClickEventForBoxes(cell){
             winner == undefined ? checkDiagonal(getDataAttrValue(cell)) : true;
         }
 
-        if (winner == undefined && clickCount == boardLength * boardLength) {
-            displayAlert("Game is tie.");
+        if (winner == undefined && clickCount == getBoardLength() * getBoardLength()) {
+            displayAlertMessage("Game is tie.");
         }
     }, 100);
-
 }
 
 // display current player after click
@@ -63,7 +96,7 @@ function displayCurrentPlayer() {
 // select a player
 function selectPlayer() {
     $('.xplayer').one('click', function () {
-        player = 'X';
+        setPlayerToken('X');
         $('#choose-player').addClass('d-none');
         displayCurrentPlayer();
     });
@@ -71,39 +104,29 @@ function selectPlayer() {
 
 // get data position value of an element
 function getDataAttrValue(elem){
-    return $(elem)[0].getAttribute('data-value')
+    return $(elem)[0].getAttribute('data-value');
 }
 
 // function to set players and display player text
-function setPlayer() {
+function changePlayer() {
     if (player==='O') {
-        player = 'X';
+        setPlayerToken('X');
     } else {
-        player = 'O';
+        setPlayerToken('O');
     }
     displayCurrentPlayer();
 }
-
-
-
-// check winner
-// function declareWinner(a, b, c) {
-//     if (a === b && b===c && a != null) {
-//         winner = a;
-//         displayAlert("Winner is: " + winner);
-//     }
-// }
 
 function declareWinner(boxValuesArray){
     let uniqValuesArray = [...new Set(boxValuesArray)];
     if(uniqValuesArray.length == 1 && uniqValuesArray[0] !=""){
         winner = uniqValuesArray[0];
-        displayAlert("Winner is: " + winner);
+        displayAlertMessage("Winner is: " + winner);
     }
 }
 
 // display alert message and load page
-function displayAlert(msg) {
+function displayAlertMessage(msg) {
     alert(msg);
     window.location.reload();
 }
@@ -134,7 +157,6 @@ function getLastIndexOfCell(dataValue){
 }
 // check diagonal
 function checkDiagonal(dataValue) {
-
     let i = getFirstIndexOfCell(dataValue);
     let j = getLastIndexOfCell(dataValue);
 
@@ -149,11 +171,11 @@ function checkDiagonal(dataValue) {
 function checkWinnerForRowColumn(checkType, index){
     let boxValuesArray = [];
     if(checkType=='row'){
-        for (let j=1; j <= boardLength; j++){
+        for (let j=1; j <= getBoardLength(); j++){
             boxValuesArray.push(getBoxValue(index+j))
         }
     }else if(checkType=='column'){
-        for (let i=1; i<=boardLength; i++){
+        for (let i=1; i<=getBoardLength(); i++){
             boxValuesArray.push(getBoxValue(i+index))
         }
     }
@@ -164,12 +186,12 @@ function checkWinnerForRowColumn(checkType, index){
 function checkWinnerForDiagonal(checkType){
     let boxValuesArray = [];
     if(checkType == 'primary-diagonal'){
-        for(let k=1; k<=boardLength; k++){
+        for(let k=1; k<=getBoardLength(); k++){
             boxValuesArray.push(getBoxValue(k.toString()+k))
         }
     }else if(checkType == 'secondary-diagonal'){
-        var SecondaryDiagonalBoxSum = parseInt(boardLength) + 1;
-        for(let k=1; k<=boardLength; k++){
+        var SecondaryDiagonalBoxSum = parseInt(getBoardLength()) + 1;
+        for(let k=1; k<=getBoardLength(); k++){
             let lastIndex = SecondaryDiagonalBoxSum - k;
             boxValuesArray.push(getBoxValue(k.toString()+ lastIndex));
         }
